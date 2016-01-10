4fc0433d5f534e6e034ea1da96f7db956c99ee17
Arya Rezavidi Viciyuda <arya.rezavidi@gmail.com>
2016-01-10 12:16:35 +0700

    Update config.js

--- configuration/config.js
+++ configuration/config.js
@@ -4,8 +4,8 @@ module.exports={
 * It consists of Twitter app information, database information.
 */
 
-	"twitter_api_key" 		: "NORoWYPU1ggeLKMzs20yQWDdb",
-	"twitter_api_secret"	: "tUfJZsO354bctK8sylViH8PC0LKFeFIRwGyLSxUHM92W7rFwwD",
+	"twitter_api_key" 		: "",
+	"twitter_api_secret"	: "",
 	"callback_url"			: "http://127.0.0.1:3000/auth/twitter/callback",
 	"use_database"			: "true",
 	"host"					: "localhost",
